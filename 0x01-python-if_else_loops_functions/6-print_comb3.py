bf0c2262a6b56672da42edfcd1155e0c822b61af
aabine <kendoaustine@yahoo.com>
2023-06-07 05:52:58 +0100

    ALX

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@
 for i in range(10):
     for j in range(i + 1, 10):
         if i < 8 and j <= 9:
-            print(f"{i}{j}", end= ", ")
+            print("{}{}".format(i, j), end=", ")
         else:
-            print(f"{i}{j}", end= "\n")
+            print("{}{}\n".format(i, j), end=" ")
