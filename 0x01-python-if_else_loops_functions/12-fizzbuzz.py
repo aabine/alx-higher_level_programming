bab8484a39640a648832c55887125d8256311c76
aabine <kendoaustine@yahoo.com>
2023-06-07 22:51:49 +0100

    ALX

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 
+result = ""
+
+
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz ".format(i), end="")
+            result = "FizzBuzz".format(i)
         elif i % 3 == 0:
-            print("Fizz ".format(i), end="")
+            result = "Fizz".format(i)
         elif i % 5 == 0:
-            print("Buzz ".format(i), end="")
+            result = "Buzz".format(i)
         else:
-            print("{} ".format(i), end="")
-    print()
+            result = "{}".format(i)
+        print("{}".format(result), end=" ")
+
+
+fizzbuzz()
