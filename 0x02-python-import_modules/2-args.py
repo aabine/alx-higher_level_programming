43b8c854f7be8dd656bd25b121be8cadc0dd3efa
aabine <kendoaustine@yahoo.com>
2023-06-09 03:59:42 +0100

    ALX

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """print arguments"""
+    import sys
+    
+    print("{} arguments:".format(len(sys.argv) - 1))
+
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
+    
