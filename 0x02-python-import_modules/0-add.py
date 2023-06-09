e5bbc01fa5345414b0db0e17603dbb5220b85e59
aabine <kendoaustine@yahoo.com>
2023-06-09 03:30:14 +0100

    ALX

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
-# Assigning values to variables a and b
-a = 1
-b = 2
-
 # Importing the add function from add_0.py
 if __name__ == "__main__":
     from add_0 import add
+
+    a = 1
+    b = 2
 
     # Calling the add function
     result = add(a, b)
