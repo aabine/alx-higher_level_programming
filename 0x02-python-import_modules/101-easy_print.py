99e4cb19b180b022f03e53034ffeac356d2912fd
aabine <kendoaustine@yahoo.com>
2023-06-09 06:01:54 +0100

    ALX

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-"""print easy with write function"""
-if __name__ == "__main__":
-    __import__('os').write(1, "#pythonisfun\n".encode('utf-8'))
+__import__('os').write(1, "#pythonisfun\n".encode('utf-8'))
