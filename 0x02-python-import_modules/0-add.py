7db5d943f95d12d72c3ce9a7c10efe30d957b65b
aabine <kendoaustine@yahoo.com>
2023-06-09 03:06:48 +0100

    ALX

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
+from add_0 import add
 
-    from add_0 import add
+a = 1
+b = 2
 
-    val1 = 1
-    val2 = 2
-
-    print("{} + {} = {}".format(val1, val2, add(val1, val2)))
+print("{} + {} = {}".format(a,b , add(a, b)))
