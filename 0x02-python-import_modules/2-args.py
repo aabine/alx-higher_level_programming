9b8cc76d9032c3a5e90338eff343cc27355806ed
aabine <kendoaustine@yahoo.com>
2023-06-09 04:24:37 +0100

    ALX

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,8 +3,12 @@
 if __name__ == "__main__":
     """print arguments"""
     import sys
-    print("{} arguments:".format(len(sys.argv) - 1))
-
+    args = len(sys.argv) - 1
+    if args == 0:
+        print("0 arguments.")
+    elif args == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(args))
     for i in range(1, len(sys.argv)):
-        result = sys.argv[i]
-        print("{}: {}".format(i, result))
+        print("{}: {}".format(i, sys.argv[i]))
