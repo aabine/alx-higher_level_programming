08d640fd1d1ce62effa801239f48925699349684
aabine <kendoaustine@yahoo.com>
2023-06-12 04:29:42 +0100

    Rplace Element in a list at any index

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    """
+    Replace an element at a specific index in a list.
+
+    Args:
+        my_list (list): The list of elements.
+        idx (int): The index where the element should be replaced.
+        element: The new element to be placed at the specified index.
+
+    Returns:
+        The modified list with the element replaced,
+        or the original list if the index is invalid.
+    """
+    if idx < 0 or idx >= len(my_list):
+        return my_list[lists]
+    my_list[idx] = element
+    return my_list
