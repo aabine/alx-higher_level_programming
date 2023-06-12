57713a23b6882dd30069f332b02fe2bb4339037a
aabine <kendoaustine@yahoo.com>
2023-06-12 20:40:07 +0100

    ALX

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -7,8 +7,9 @@ def new_in_list(my_list, idx, element):
     It returns a new list with the element at the specified index.
     The original list is not modified.
     """
-    if idx < 0 or idx > len(my_list):
-        return my_list
-    n_list = my_list.copy()
-    n_list[idx] = element
+    if my_list:
+        if idx < 0 or idx > len(my_list):
+            return my_list
+        n_list = my_list.copy()
+        n_list[idx] = element
     return n_list
