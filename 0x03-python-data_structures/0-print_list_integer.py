295c9777ce6fb1e853fdb7c28393ee1d96b7941f
aabine <kendoaustine@yahoo.com>
2023-06-12 03:00:56 +0100

    Print a list in python

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    """
+    Print the elements of a list on separate lines.
+
+    Args:
+        my_list (list): The list of elements to be printed.
+    """
+    for lists in range(len(my_list)):
+        print("{}".format(my_list[lists]))
