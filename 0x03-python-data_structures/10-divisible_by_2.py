67ff76a874e1cf2d5bb8b6f5ebcba27b0a06122e
aabine <kendoaustine@yahoo.com>
2023-06-13 03:29:56 +0100

    ALX

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+    This function takes a list of integers as input and returns a new list
+    containing only the even numbers in the original list. If the original
+    list is empty, the function returns the original list.
+
+    Args:
+        my_list (list): A list of integers to be analyzed.
+
+    Returns:
+        list: A new list containing only the even numbers in the original list.
+    """
+
+
+def divisible_by_2(my_list=[]):
+    if my_list:
+        n_list = []
+        for index in my_list:
+            if index % 2 == 0:
+                n_list.append(index)
+        return n_list
+    else:
+        return my_list
