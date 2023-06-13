75d90a8867a938686303e9a4839e90dc5f9f73c0
aabine <kendoaustine@yahoo.com>
2023-06-13 03:00:47 +0100

    ALX

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    """
+    Returns a tuple containing the length of the input
+    string and its first character, or a tuple containing a single value
+    of zero if the input string is empty.
+
+    Parameters:
+    sentence (str): A string to be analyzed.
+
+    Returns:
+    tuple: A tuple containing the length of the input string
+    and its first character, or a tuple containing a single value
+    of zero if the input string is empty.
+
+    """
+    if sentence:
+        length = len(sentence)
+        first_char = sentence[0]
+        return length, first_char
+    else:
+        return 0, "None"
+    
