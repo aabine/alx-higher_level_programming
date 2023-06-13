eea40bb0353d64658868618caa4d176be07ab353
aabine <kendoaustine@yahoo.com>
2023-06-13 03:56:59 +0100

    ALX

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -16,6 +16,7 @@
         list: A new list with the element at the given index removed.
 """
 
+
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
