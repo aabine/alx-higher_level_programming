5c7cee1d2d211ec1ecff4eed4369b524357fb085
aabine <kendoaustine@yahoo.com>
2023-06-13 03:49:58 +0100

    ALX

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -17,9 +17,9 @@
 """
 
 def delete_at(my_list=[], idx=0):
+    n_list = my_list.copy()
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
     else:
-        n_list = my_list.copy()
         del n_list[idx]
-    return n_list
+        return n_list
