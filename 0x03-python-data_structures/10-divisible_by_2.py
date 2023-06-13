2525d70346b24a9aa46f7575db9e630119760b1b
aabine <kendoaustine@yahoo.com>
2023-06-13 03:34:22 +0100

    ALX

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -16,9 +16,11 @@
 def divisible_by_2(my_list=[]):
     if my_list:
         n_list = []
-        for index in my_list:
-            if index % 2 == 0:
-                n_list.append(index)
+        for index in range(len(my_list)):
+            if my_list[index] % 2 == 0:
+                n_list.append(True)
+            else:
+                n_list.append(False)
         return n_list
     else:
         return my_list
