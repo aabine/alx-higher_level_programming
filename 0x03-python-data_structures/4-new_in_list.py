359d25d59d73264bf3b4d1188e03bf0939fc7a22
aabine <kendoaustine@yahoo.com>
2023-06-13 01:36:23 +0100

    ALX

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -10,6 +10,7 @@ def new_in_list(my_list, idx, element):
     if my_list:
         if idx < 0 or idx > len(my_list):
             return my_list
-        n_list = my_list.copy()
-        n_list[idx] = element
-    return n_list
+        else:
+            n_list = my_list[:]
+            n_list[idx] = element
+            return n_list
