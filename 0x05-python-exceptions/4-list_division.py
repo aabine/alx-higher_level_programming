cd9c34bbd6edaad3343c7a1396265cc69cbdddcd
aabine <kendoaustine@yahoo.com>
2023-06-28 03:07:50 +0100

    Correction

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -18,7 +18,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             print("division by 0")
             result = 0
-        except (ValueError, IndexError, AttributeError):
+        except (IndexError):
             print("out of range")
             result = 0
         except TypeError:
