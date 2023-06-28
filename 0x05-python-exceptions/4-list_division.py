dde80cebaae6c9812ece2a6b9d267f3158d23895
Kendoaustine <kendoaustine@yahoo.com>
2023-06-28 06:59:58 +0100

    Update 4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,15 +1,14 @@
 #!!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    """
-    Safely divides two numbers and returns the result. If a zero division error or a value error
-    occurs during the division, it returns None. The function takes two parameters:
+"""Divides two lists element by element.
 
     Args:
-    - my_list_1 (list): The numerator of the division.
-    - my_list_2 (list): The denominator of the division. It must not be zero.
+        my_list_1 (list): The first list.
+        my_list_2 (list): The second list.
+        list_length (int): The number of elements to divide.
 
     Returns:
-    - list: The results of the division. Each element in the list corresponds to the division of the corresponding elements from my_list_1 and my_list_2. If an error occurs during the division, the corresponding element in the result list is None.
+        A new list of length list_length containing all the divisions.
     """
 
     results = []
