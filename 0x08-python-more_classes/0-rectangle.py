4cb0be5f011168558fc524773e625512c2fc83af
Kendoaustine <kendoaustine@yahoo.com>
2023-07-04 07:03:50 +0100

    Update 0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -4,8 +4,11 @@
 Class definition
 """
 
+# Define a class called Rectangle
 class Rectangle:
 
-	"""An empty rectangle class"""
-
-		pass
+    """An empty rectangle class"""
+    
+    # Since the class doesn't have any attributes or methods yet,
+    # we use the 'pass' statement to indicate that it's intentionally empty.
+    pass
