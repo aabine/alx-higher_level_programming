2f6f18abb28c35054718ece38428ee567fb9ad96
aabine <kendoaustine@yahoo.com>
2023-07-18 04:22:44 +0100

    PYthon Class

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -246,9 +246,9 @@ class TestBase_from_json_string(unittest.TestCase):
 
     def test_from_json_string_two_rectangles(self):
         list_input = [
-            {"id": 89, "width": 10, "height": 4, "x": 7, "y": 8},
-            {"id": 98, "width": 5, "height": 2, "x": 1, "y": 3},
-        ]
+                {"id": 89, "width": 10, "height": 4, "x": 7, "y": 8},
+                {"id": 98, "width": 5, "height": 2, "x": 1, "y": 3},
+                ]
         json_list_input = Rectangle.to_json_string(list_input)
         list_output = Rectangle.from_json_string(json_list_input)
         self.assertEqual(list_input, list_output)
@@ -261,9 +261,9 @@ class TestBase_from_json_string(unittest.TestCase):
 
     def test_from_json_string_two_squares(self):
         list_input = [
-            {"id": 89, "size": 10, "height": 4},
-            {"id": 7, "size": 1, "height": 7}
-        ]
+                {"id": 89, "size": 10, "height": 4},
+                {"id": 7, "size": 1, "height": 7}
+                ]
         json_list_input = Square.to_json_string(list_input)
         list_output = Square.from_json_string(json_list_input)
         self.assertEqual(list_input, list_output)
