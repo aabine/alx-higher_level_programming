8d9553c72297cb5531cdbc286e8b81930041d859
aabine <kendoaustine@yahoo.com>
2023-07-25 07:06:42 +0100

    MINE

--- practice-python/c-project/1-insertion_sort_list.c
+++ practice-python/c-project/1-insertion_sort_list.c
@@ -8,36 +8,36 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current_node, *sorted_list, *temp_node;
+	listint_t *current_node, *sorted_list, *temp_node;
 
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-    sorted_list = *list;
-    current_node = sorted_list->next;
+	sorted_list = *list;
+	current_node = sorted_list->next;
 
-    while (current_node != NULL)
-    {
-        while (current_node->prev != NULL && current_node->n < current_node->prev->n)
-        {
-            temp_node = current_node->prev;
-            temp_node->next = current_node->next;
+	while (current_node != NULL)
+	{
+		while (current_node->prev != NULL && current_node->n < current_node->prev->n)
+		{
+			temp_node = current_node->prev;
+			temp_node->next = current_node->next;
 
-            if (current_node->next != NULL)
-                current_node->next->prev = temp_node;
+			if (current_node->next != NULL)
+				current_node->next->prev = temp_node;
 
-            current_node->next = temp_node;
-            current_node->prev = temp_node->prev;
-            temp_node->prev = current_node;
+			current_node->next = temp_node;
+			current_node->prev = temp_node->prev;
+			temp_node->prev = current_node;
 
-            if (current_node->prev != NULL)
-                current_node->prev->next = current_node;
-            else
-                *list = current_node;
-            
-            print_list(*list);
-        }
+			if (current_node->prev != NULL)
+				current_node->prev->next = current_node;
+			else
+				*list = current_node;
 
-        current_node = current_node->next;
-    }
+			print_list(*list);
+		}
+
+		current_node = current_node->next;
+	}
 }
