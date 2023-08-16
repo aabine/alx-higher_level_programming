a32bb9ceb474a333d5395590ddcba4732add8b20
aabine <kendoaustine@yahoo.com>
2023-08-16 20:26:14 +0100

    SQL

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,9 +1,9 @@
 -- Select the city and maximum temperature for each city
 -- during the months of July and August
 
-SELECT city, MAX(value) AS max_temperature
+SELECT city, MAX(value) AS max_temp
 FROM temperatures
 WHERE MONTH(month_data) IN (7, 8)
 GROUP BY city
-ORDER BY max_temperature DESC
+ORDER BY max_temp DESC
 LIMIT 3;
--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,7 +1,7 @@
 -- This query retrieves the maximum temperature
 -- for each state from the 'temperatures' table
 
-SELECT state, MAX(value) AS max_temperature
+SELECT state, MAX(value) AS max_temp
 FROM temperatures
 GROUP BY state
 ORDER BY state;
