a45ad36de1e65c2b854604fb141114a79e5ddcd9
aabine <kendoaustine@yahoo.com>
2023-08-16 20:21:59 +0100

    SQL

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 -- Select the city and maximum temperature for each city
 -- during the months of July and August
 
-SELECT city, MAX(temp) AS max_temperature
+SELECT city, MAX(value) AS max_temperature
 FROM temperatures
 WHERE MONTH(month_data) IN (7, 8)
 GROUP BY city
