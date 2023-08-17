86d8ff37bf0172ac5a8b4ffabc30c49f16fd8264
aabine <kendoaustine@yahoo.com>
2023-08-17 04:16:09 +0100

    SQL 2

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,12 +1,8 @@
--- This query retrieves the genres and the number of
--- shows for each genre from the TV genres table.
--- It joins the TV genres table with the TV show 
--- genres table using the genre ID.
--- The result is grouped by genre and ordered by
--- the number of shows in descending order.
+-- Retrieves all genres from the hbtn_0d_tvshows table and displays the number of shows associated with each genre.
+-- Retrieves all rows from the database that meet a specified condition.
 
-SELECT tv_genres.genre, COUNT(tv_show_genres.show_id) AS number_of_shows
-FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genres.genre
+SELECT tv_genres.name AS 'genre', COUNT(tv_show_genres.genre_id) AS 'number_of_shows'
+FROM tv_genres RIGHT JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
 ORDER BY number_of_shows DESC;
--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,19 +1,7 @@
--- This SQL query retrieves the titles of TV shows that belong to the 'Comedy' genre, ordered alphabetically.
-
--- SELECT statement to retrieve the title column from the tv_shows table.
--- Alias the table as 'tv_shows'.
-SELECT tv_shows.title
-
--- JOIN tv_show_genres table with tv_shows table using the id column from both tables.
--- Alias the table as 'tv_show_genres'.
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-
--- JOIN tv_genres table with tv_show_genres table using the genre_id column from both tables.
--- Alias the table as 'tv_genres'.
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-
--- WHERE clause to filter the results to only include the genre with the name 'Comedy'.
-WHERE tv_genres.name = 'Comedy'
-
--- ORDER BY clause to sort the results in ascending order based on the title column.
-ORDER BY tv_shows.title ASC;
+-- lists all shows, and all genres linked to that show, in the database hbtn_0d_tvshows
+-- lists all rows of a table linked to another table from the database hbtn_0d_tvshows
+SELECT title, name
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY title ASC, name ASC;
