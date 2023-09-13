fcb31695b036744521c1807e3137455306aa7414
aabine <kendoaustine@yahoo.com>
2023-09-13 20:17:08 +0100

    JavaScript Correction

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
-function findSecondBiggestInteger(args) {
-    const numbers = args.map(Number);
-    const uniqueNumbers = [...new Set(numbers)];
-  
-    if (uniqueNumbers.length < 2) {
-      return 0;
-    }
-  
-    const sortedNumbers = uniqueNumbers.sort((a, b) => b - a);
-    return sortedNumbers[1];
+function findSecondBiggestInteger (args) {
+  const numbers = args.map(Number);
+  const uniqueNumbers = [...new Set(numbers)];
+
+  if (uniqueNumbers.length < 2) {
+    return 0;
   }
-  
-  const input = process.argv.slice(2);
-  const result = findSecondBiggestInteger(input);
-  console.log(result);
+
+  const sortedNumbers = uniqueNumbers.sort((a, b) => b - a);
+  return sortedNumbers[1];
+}
+
+const input = process.argv.slice(2);
+const result = findSecondBiggestInteger(input);
+console.log(result);
