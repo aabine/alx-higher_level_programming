f881b1a2617fa82e0cb4c8d0347faab938d31221
aabine <kendoaustine@yahoo.com>
2023-09-14 04:00:41 +0100

    ALX

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,18 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-const destinationFile = process.argv[4];
-
-fs.readFile(process.argv[2], (err, content1) => {
-  if (err) throw err;
-
-  fs.readFile(process.argv[3], (err, content2) => {
-    if (err) throw err;
-
-    const concatenatedContent = Buffer.concat([content1, content2]);
-
-    fs.writeFile(destinationFile, concatenatedContent, (err) => {
-      if (err) throw err;
-    });
-  });
-});
+const fArg = fs.readFileSync(process.argv[2], 'utf8');
+const sArg = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], fArg + sArg);
