f31f00dd05d6e86556b2283b3d736b01c4775bd8
aabine <kendoaustine@yahoo.com>
2023-09-14 03:08:37 +0100

    ALX

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const oldSquare = require('./5-square');
 
-class Square extends Rectangle {
+class Square extends oldSquare {
   constructor (size) {
     super(size, size);
   }
