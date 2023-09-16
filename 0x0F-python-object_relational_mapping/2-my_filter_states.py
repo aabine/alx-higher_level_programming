b2956c476dfc03b5081d09721c2e78c31872ed1b
aabine <kendoaustine@yahoo.com>
2023-09-16 08:25:01 +0100

    ORM

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,33 +1,17 @@
 #!/usr/bin/python3
-"""
-Print all states whose names match a provided argument.
-"""
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    try:
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3]
-        )
-        state_name = sys.argv[4]
-
-        query = """
-            SELECT * FROM states
-            WHERE name LIKE BINARY %s
-            ORDER BY states.id
-        """
-
-        with db.cursor() as cursor:
-            cursor.execute(query, (state_name,))
-            rows = cursor.fetchall()
-            for row in rows:
-                print(row)
-    except Exception as e:
-        print("Error:", e)
-    finally:
-        db.close()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
