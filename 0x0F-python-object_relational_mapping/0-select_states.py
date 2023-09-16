e3d7cee5d2867953c4d1c3fa5806b98faeae0944
aabine <kendoaustine@yahoo.com>
2023-09-16 03:13:44 +0100

    ALX

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,16 @@
+#!/usr/bin/python3
+"""Script that lists all State objects from the database hbtn_0e_6_usa"""
 import MySQLdb
-import sys
 
 if __name__ == "__main__":
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=database_name
-    )
-
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY states.id")
-    rows = cursor.fetchall()
-
-    for row in rows:
-        print(row)
-
-    cursor.close()
-    db.close()
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                             passwd=sys.argv[2], db=sys.argv[3])
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM states")
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+        cursor.close()
+    except MySQLdb.Error as e:
+        print("An error occurred while connecting to the database:", e)
