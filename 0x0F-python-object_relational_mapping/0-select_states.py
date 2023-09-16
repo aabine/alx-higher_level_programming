38780c1e45a7cec103d7527426bd0dcf3db4b2c0
aabine <kendoaustine@yahoo.com>
2023-09-16 06:09:51 +0100

    ALX

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Script that lists all State objects from the database hbtn_0e_6_usa"""
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
