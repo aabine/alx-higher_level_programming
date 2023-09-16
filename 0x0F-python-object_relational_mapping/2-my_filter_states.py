c77a1d7996a86f043b3bf855144bdf6eaa2ea019
aabine <kendoaustine@yahoo.com>
2023-09-16 08:36:12 +0100

    ORM

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,33 +2,28 @@
 """
 Print all states whose names match a provided argument.
 """
-
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    try:
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3]
-        )
-        state_name = sys.argv[4]
+# Connect to the MySQL server
+connection = MySQLdb.connect(host="localhost",port=3306,
+                             user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-        query = """
-            SELECT * FROM states
-            WHERE name LIKE BINARY '{}'
-            ORDER BY states.id
-        """.format(state_name)
+# Create a cursor object
+cursor = connection.cursor()
 
-        with db.cursor() as cursor:
-            cursor.execute(query)
-            rows = cursor.fetchall()
-            for row in rows:
-                print(row)
-    except Exception as e:
-        print("Error:", e)
-    finally:
-        db.close()
+# Execute the SQL query
+query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY states.id"
+state_name = sys.argv[4]
+cursor.execute(query, (state_name,))
+
+# Fetch all the rows
+rows = cursor.fetchall()
+
+# Print each row
+for row in rows:
+    print(row)
+
+# Close the cursor and database connection
+cursor.close()
+connection.close()
