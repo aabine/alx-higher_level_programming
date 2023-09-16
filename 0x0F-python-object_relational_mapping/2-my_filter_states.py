4873fc5bd6f97ef4dee0cc47662684b0c5ceeed0
aabine <kendoaustine@yahoo.com>
2023-09-16 08:13:15 +0100

    ORM

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Print all states passed as an argument."""
+"""
+Print all states whose names match a provided argument.
+"""
+
 import MySQLdb
 import sys
 
@@ -12,11 +15,16 @@ if __name__ == "__main__":
             passwd=sys.argv[2],
             db=sys.argv[3]
         )
+        state_name = sys.argv[4]
+
+        query = """
+            SELECT * FROM states
+            WHERE name LIKE BINARY %s
+            ORDER BY states.id
+        """
+
         with db.cursor() as cursor:
-            cursor.execute(
-                "SELECT * FROM states WHERE name LIKE BINARY %s",
-                format(sys.argv[4],)
-            )
+            cursor.execute(query, (state_name,))
             rows = cursor.fetchall()
             for row in rows:
                 print(row)
