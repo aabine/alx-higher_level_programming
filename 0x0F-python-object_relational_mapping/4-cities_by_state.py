edb08924d062ec4e64c43b1f751c98f9a9729e0e
aabine <kendoaustine@yahoo.com>
2023-09-16 18:35:34 +0100

    ORM

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" List all cities from the database hbtn_0e_4_usa """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        )
+        with db.cursor() as cursor:
+            cursor.execute("""SELECT cities.id, cities.name, states.name
+                            FROM cities INNER JOIN states
+                            ON cities.state_id = states.id
+                            ORDER BY cities.id
+                            """)
+            rows = cursor.fetchall()
+            for row in rows:
+                print(row)
+    except Exception as e:
+        print("Error:", e)
+    finally:
+        db.close()
