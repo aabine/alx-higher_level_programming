43392a8b5ad85ae14e93f160100fc9d29305de52
aabine <kendoaustine@yahoo.com>
2023-09-16 03:10:54 +0100

    ALX

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,34 @@
-#!/usr/bin/python3
-"""Script that lists all State objects from the database hbtn_0e_6_usa"""
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    try:
-        db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                             passwd=sys.argv[2], db=sys.argv[3])
-        cursor = db.cursor()
-        cursor.execute("SELECT * FROM states")
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
-        cursor.close()
-    except MySQLdb.Error as e:
-        print("An error occurred while connecting to the database:", e)
+    # Get MySQL credentials from command line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    # Connect to the MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+    )
+
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
+
+    # Execute the SQL query to retrieve states sorted by states.id
+    cursor.execute("SELECT * FROM states ORDER BY states.id")
+
+    # Fetch all the rows
+    rows = cursor.fetchall()
+
+    # Display the results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
