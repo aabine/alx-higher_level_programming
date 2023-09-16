8e4caabac53cddd6b72f6dfbfdb1f2cfc0513778
aabine <kendoaustine@yahoo.com>
2023-09-16 07:09:40 +0100

    ORM

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,13 +5,11 @@
 import MySQLdb
 
 if __name__ == "__main__":
-    try:
-        db = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="", db="hbtn_0e_0_usa")
-        cursor = db.cursor()
-        cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
-        cursor.close()
-    except MySQLdb.Error as e:
-        print("An error occurred while connecting to the database:", e)
+    db = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="", db="hbtn_0e_0_usa")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
