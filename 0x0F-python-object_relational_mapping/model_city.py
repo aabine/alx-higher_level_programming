35b300321e499aaeb2546cee73ba902548c9c599
aabine <kendoaustine@yahoo.com>
2023-09-18 02:03:00 +0100

    ALx

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -6,8 +6,10 @@ from sqlalchemy import Column, Integer, String, MetaData, ForeignKey
 metaData = MetaData()
 Base = declarative_base(metadata=metaData)
 
+
 class City(Base):
+    """ City class with name and id attributes """
     __tablename__ = 'cities'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'),nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
