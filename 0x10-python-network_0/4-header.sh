749a25ad724392d49d87af898e4283a716545511
aabine <kendoaustine@yahoo.com>
2023-09-30 02:09:26 +0100

    fix

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-# Check if a URL is provided as an argument
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
 # Define the URL and header variable
 URL="$1"
 HEADER="X-School-User-Id: 98"
