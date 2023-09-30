d452f7293e57210e62454a4ab3183435f1012eaa
aabine <kendoaustine@yahoo.com>
2023-09-30 02:01:47 +0100

    fixed

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Sends a POST request to 0.0.0.0:5000/catch_me with specific headers and data to get the message "You got me!".
-
-curl -sL -X POST -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
+# Send a POST request to 0.0.0.0:5000/catch_me with a custom header
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
