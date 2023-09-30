aa31049c1823fd90a9e2ba0c4d45edd0d25cbb0d
aabine <kendoaustine@yahoo.com>
2023-09-30 02:06:08 +0100

    Fixed

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-# Send a GET request to the specified URL with a custom header variable.
-curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
+
+# Check if a URL is provided as an argument
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <URL>"
+  exit 1
+fi
+
+# Define the URL and header variable
+URL="$1"
+HEADER="X-School-User-Id: 98"
+
+# Send the GET request with the specified header using curl and display the response body
+curl -s -H "$HEADER" "$URL"
