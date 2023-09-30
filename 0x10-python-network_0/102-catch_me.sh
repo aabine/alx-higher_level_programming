4e707dfc019d464a9e6e4d6ff9d9bcb20f22b763
aabine <kendoaustine@yahoo.com>
2023-09-30 01:57:38 +0100

    UPDATE

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Sends a PUT request to 0.0.0.0:5000/catch_me with specific headers and data to get the message "You got me!".
-curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
+# Sends a POST request to 0.0.0.0:5000/catch_me with specific headers and data to get the message "You got me!".
+
+curl -sL -X POST -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
