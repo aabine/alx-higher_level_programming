811eaa5e62f188a829268dde2ce5d8a04af440a4
aabine <kendoaustine@yahoo.com>
2023-11-02 04:09:56 +0100

    jquery

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,5 @@
+$.getJSON('https://swapi-api.alx-tools.com/api/films/?format=json', function(data) {
+  $.each(data.results, function(index, movie) {
+    $('<li>').text(movie.title).appendTo('#list_movies');
+  });
+});
