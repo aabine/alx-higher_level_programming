3f39b9d2661bf2e74c60c1c7f6d8c47239639fde
aabine <kendoaustine@yahoo.com>
2024-02-13 16:24:24 +0100

    Update

--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-if (process.argv.length[2] === undefined) {
-  console.log(process.argv[2] + ' is ' + process.argv[3]);
-} else if (process.argv.length > 2) {
-  console.log(process.argv[2] + ' is ' + process.argv[3]);
-}
--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const convertedArg = parseInt(process.argv[2]);
-
-if (isNaN(convertedArg)) {
-  console.log('Not a number');
-} else {
-  console.log('My number: ' + convertedArg);
-}
